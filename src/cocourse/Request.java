bd622e4b125e5e2839498d2476bd00cdba356938
decantr <theeasterlywood@gmail.com>
2018-04-29 03:17:04 +0100

    Request now handles closing socket

--- src/cocourse/Request.java
+++ src/cocourse/Request.java
@@ -8,34 +8,47 @@ import java.net.Socket;
 class Request implements Runnable {
 
 	private Socket i;
+	private ServerBack s;
+	private BufferedReader r;
+	private PrintWriter o;
 
-	Request ( Socket i ) {
+	Request ( ServerBack s , Socket i ) {
+		this.s = s;
 		this.i = i;
 	}
 
+	public void close () {
+		System.out.println( "shutting down" );
+		try {
+			i.close( );
+		} catch ( Exception ignored ) {
+		}
+	}
 
 	@Override
-	public void run() {
+	public void run () {
 		try {
 
-			BufferedReader r = new BufferedReader( new InputStreamReader( i.getInputStream() ) );
+			r = new BufferedReader( new InputStreamReader( i.getInputStream( ) ) );
 
-			PrintWriter o = new PrintWriter( i.getOutputStream() );
+			o = new PrintWriter( i.getOutputStream( ) );
 
-			boolean c = true;
+			o.println( "connected" + i.getInetAddress( ) );
 
-			while ( c ) {
+			while ( true ) {
 
-				String t = r.readLine();
+				String t = r.readLine( );
 
-				if ( t != null ) System.out.println( t );
+				if ( t == null || t.equals( "exit" ) ) break;
+
+				if ( t.equals( "b" ) ) System.out.println( t );
 
 			}
 
 		} catch ( Exception e ) {
 			System.out.println( e );
 		} finally {
-			try { i.close(); } catch ( Exception ignored ) {}
+			s.close( this );
 		}
 	}
 }
